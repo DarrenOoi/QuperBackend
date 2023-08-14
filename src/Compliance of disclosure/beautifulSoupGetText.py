aaa3c2a20e4b29fe1aa2791ea7b810acff6801e9
yanhaha <86403644+YanChuan390@users.noreply.github.com>
2023-08-14 15:41:55 +1000

    Update beautifulSoupGetText.py

--- src/Compliance of disclosure/beautifulSoupGetText.py
+++ src/Compliance of disclosure/beautifulSoupGetText.py
@@ -7,11 +7,11 @@ import joblib
 # from bayesian import clf,tf
 from bs4 import BeautifulSoup
 
-# mark_txt = {'0':"personal_information_type.txt",'1':"personal_information_type.txt",'2':"personal_information_type.txt",
-#             '3':"share_information.txt",'4':"protect_information.txt",
-#             '5':"advertising.txt",'6':"user_right.txt",'7':"special_group.txt",
-#             '8':"special_area.txt",'9':"update.txt",'10':"way_to_collect.txt",
-#             '11':"provider.txt",'12':"data_retention.txt",'13':"personal_information_type.txt",'14':"thrid_party.txt",'15':"personal_infoinformation_tyoe.txt"}
+mark_txt = {'0':"personal_information_type.txt",'1':"personal_information_type.txt",'2':"personal_information_type.txt",
+             '3':"share_information.txt",'4':"protect_information.txt",
+             '5':"advertising.txt",'6':"user_right.txt",'7':"special_group.txt",
+             '8':"special_area.txt",'9':"update.txt",'10':"way_to_collect.txt",
+             '11':"provider.txt",'12':"data_retention.txt",'13':"personal_information_type.txt",'14':"thrid_party.txt",'15':"personal_infoinformation_tyoe.txt"}
 clf = joblib.load('bys_classifier.pkl')
 tf = joblib.load('bys_tf.pkl')
 def pre_title(title_list):
