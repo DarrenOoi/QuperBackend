9b094f3bc1d28fb30ec161702ddf67bfe5ddd149
Darren Ooi <darrentyooi@gmail.com>
2023-09-12 15:45:19 +1000

    adjust timeline.py sleep to WebDriverWait

--- src/Compliance of disclosure/timeline.py
+++ src/Compliance of disclosure/timeline.py
@@ -4,106 +4,85 @@ import re
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 from selenium.webdriver.chrome.options import Options
 from waybackpy import WaybackMachineSaveAPI
 USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36"
+
+
 def getFrequency(url):
-    # chrome_driver_path = '/Users/chuan/chrome-mac-arm64'
-    # bor = webdriver.Chrome(executable_path='chromedriver')
-    # chrome_options = webdriver.ChromeOptions()
-    # bor = webdriver.Chrome(options=chrome_options)
     options = Options()
     options.add_argument('--headless')
-    # options.headless = True
     bor = webdriver.Chrome(options=options)
     bor.maximize_window()
     bor.get('https://archive.org/web/')
-    # bor.get('https://www.getbring.com/en/privacy-policy')
     # positioning input boxes
-    input_box = bor.find_element(by=By.ID,value='wwmurl')
+    input_box = bor.find_element(by=By.ID, value='wwmurl')
     try:
         input_box.send_keys(url)
     except Exception:
         pass
     # locate the search button
-    button = bor.find_element(by=By.NAME , value='type')
+    button = bor.find_element(by=By.NAME, value='type')
     try:
         button.click()
     except Exception:
         pass
 
-    sleep(5)
+    sleep(3)
     startTime = None
     endTime = None
     captures = None
     duplicates = None
     uniques = None
-    # newOne = None
-    # locating the timestamp location
-    # try:
-    #     times = bor.find_element(by=By.CLASS_NAME , value='captures-range-info').find_element(by=By.TAG_NAME , value='strong').text
-    #     interval = bor.find_element(by=By.CLASS_NAME , value='captures-range-info').find_elements(by=By.TAG_NAME , value='a')
-    #     startTime = interval[0].text
-    #     endTime = interval[1].text
-    #     # save_api = WaybackMachineSaveAPI(url, USER_AGENT)
-    #     # save_api.save()
-    #     # newOne = save_api.timestamp()
-    # except Exception:
-    #     print(111)
-    #     pass
     try:
-        url_button = bor.find_element(by=By.ID, value='react-wayback-search').find_element(by=By.CLASS_NAME,value='view-navbar').find_elements(by=By.TAG_NAME,value='a')
-        # url_button = bor.find_element(by=By.ID,value='react-wayback-search').find_element(by=By.CLASS_NAME,value='view-navbar').find_element(by=By.CLASS_NAME,value='navbar-option beta-badge-container selected-option')
+        url_button = bor.find_element(by=By.ID, value='react-wayback-search').find_element(
+            by=By.CLASS_NAME, value='view-navbar').find_elements(by=By.TAG_NAME, value='a')
         url_button[-1].click()
     except:
         pass
-        print("not find urlbutton")
+        print("could not find urlbutton")
 
-    sleep(5)
+        # Create a WebDriverWait instance with a maximum wait time of 30 seconds (adjust as needed)
+    wait = WebDriverWait(bor, 100)
 
     try:
-        root = bor.find_element(by=By.ID,value='url-query-result').find_element(by=By.ID,value='resultsUrl_wrapper').find_elements(by=By.CLASS_NAME,value='row')[1].find_element(by=By.CLASS_NAME,value='col-sm-12').find_element(by=By.ID,value='resultsUrl').find_element(by=By.TAG_NAME,value='tbody').find_element(by=By.CLASS_NAME,value='odd')
-        # startTime = root.find_element(by=By.CLASS_NAME,value='dateFrom').text
-        # endTime = root.find_element(by=By.CLASS_NAME,value='dateTo').text
-        # captures = root.find_element(by=By.CLASS_NAME,value='captures text-center').text
-        # duplicates = root.find_element(by=By.CLASS_NAME,value='dupes text-center').text
-        # uniques = root.find_element(by=By.CLASS_NAME,value='uniques text-center').text
-        alldata = root.find_elements(by=By.TAG_NAME,value='td')
+        wait.until(lambda bor: len(bor.find_elements(
+            By.CSS_SELECTOR, '#resultsUrl tbody tr')) > 1)
+        root = wait.until(EC.presence_of_all_elements_located(
+            (By.CSS_SELECTOR, '#url-query-result #resultsUrl_wrapper .row:nth-child(2) .col-sm-12 #resultsUrl tbody .odd')))
+        alldata = root[0].find_elements(By.TAG_NAME, 'td')
+
         startTime = alldata[2]
         endTime = alldata[3]
         captures = alldata[4]
         duplicates = alldata[5]
         uniques = alldata[6]
-    except:
-        pass
-        print("not find content")
-    # print(startTime)
-    # print(endTime)
-    # print(captures)
-    # print(duplicates)
-    # print(uniques)
+    except Exception as e:
+        print("Could not find content:", str(e))
+
     timeline = {
-        "start" : startTime.text,
-        "end" : endTime.text,
-        "captures" : captures.text,
-        "duplicates" : duplicates.text,
-        "uniques" : uniques.text,
+        "start": startTime.text if startTime else None,
+        "end": endTime.text if endTime else None,
+        "captures": captures.text if captures else None,
+        "duplicates": duplicates.text if duplicates else None,
+        "uniques": uniques.text if uniques else None,
     }
 
     return timeline
-    # return startTime.text , endTime.text , captures.text,duplicates.text,uniques.text
 
-# getFrequency("https://www.energyhub.com/privacy")
-# getFrequency('https://www.allegion.com/corp/en/footer/privacy-statement/tr.html')
+
 def Find(string):
     # findall() Finding strings that match a regular expression
     url = re.findall('https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+', string)
     return url
 
+
 def Find_kuohao(string):
     p1 = re.compile(r'[(](.*?)[)]', re.S)
-    list1 = re.findall(p1,string)
+    list1 = re.findall(p1, string)
     # print(list1)
     result = []
     for i in list1:
@@ -113,4 +92,5 @@ def Find_kuohao(string):
         result = list(set(result))
     return result
 
-print(getFrequency("https://explore.zoom.us/en/privacy/"))
+
+# print(getFrequency("https://explore.zoom.us/en/privacy/"))
